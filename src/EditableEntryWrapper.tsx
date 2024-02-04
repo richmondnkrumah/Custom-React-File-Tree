9413f3a5a8225471d5d5b3a8a2b13d9a3f39a352
Void <richmondnkrumah04@gmail.com>
2024-02-04 01:55:34 +0000

    HOC component that wraps UI component

---
+++ src/EditableEntryWrapper.tsx
@@ -0,0 +1,56 @@
+// EditableEntryWrapper.tsx
+import React, {
+  useState,
+  useRef,
+  useEffect,
+} from "react";
+import { useExplorerFileTree } from "./store";
+
+interface EditableEntryWrapperProps {
+  isFolder?: boolean;
+  handleIsDone:   () => void
+}
+
+const EditableEntryWrapper: React.FC<EditableEntryWrapperProps> = ({
+  isFolder,handleIsDone
+}) => {
+  const [isEditing, setIsEditing] = useState<boolean>(true);
+  const [inputValue, setInputValue] = useState<string>("");
+  const inputRef = useRef<HTMLInputElement>(null);
+  const {createNode} = useExplorerFileTree()
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value);
+  };
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      setIsEditing(false);
+      createNode(inputValue,isFolder)
+      handleIsDone()
+    }
+  };
+  useEffect(() => {
+    inputRef.current?.focus({});
+  });
+
+  return (
+    isEditing && (
+      <div>
+        {isFolder ? <span>true</span> : <span>false</span>}
+        {isEditing && (
+          <input
+            className="bg-red-300"
+            ref={inputRef}
+            type="text"
+            value={inputValue}
+            onKeyDown={handleKeyDown}
+            onChange={handleInputChange}
+            disabled={!isEditing}
+          />
+        )}
+      </div>
+    )
+  );
+};
+
+export default EditableEntryWrapper;
