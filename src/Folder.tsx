13988e9a3066c174bb934799309f854045354b80
Void <richmondnkrumah04@gmail.com>
2024-02-04 01:55:04 +0000

    UI Folder component

---
+++ src/Folder.tsx
@@ -0,0 +1,20 @@
+import { useExplorerFileTree } from "./store";
+type props = {
+  name?: string;
+};
+
+const Folder = ({ name }: props) => {
+  const {setCurrentFolderNode } = useExplorerFileTree();
+
+  const handleFolderClick = () => {
+    console.log("I am clicked")
+    setCurrentFolderNode(name!);
+  };
+  return (
+    <div style={{background: 'red',padding:"1px"}} onClick={handleFolderClick} >
+      <p>{name}</p>
+    </div>
+  );
+};
+
+export default Folder;
