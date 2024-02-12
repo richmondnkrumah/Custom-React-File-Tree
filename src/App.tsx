9e7bed74b52f3366dfa92e301d8c71499081b20b
Void <richmondnkrumah04@gmail.com>
2024-02-12 02:42:19 +0000

    integrated the createNode component

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,27 @@
-import { useState } from "react";
+import { useState,useRef, MutableRefObject, ReactNode } from "react";
 import EditableEntryWrapper from "./EditableEntryWrapper";
 import Entry from "./Entry";
 import { useExplorerFileTree } from "./store";
+import CreateNode from './CreateNode'
 
 const App = () => {
-  const { FileNodes, currentFolderNode } =
+  const { FileNodes, currentFolderNode,setNewNode } =
     useExplorerFileTree();
   const [folState, setFolState] = useState<null | boolean>(null);
   const [isDone, setIsDone] = useState<boolean>(true);
+  const treeNodeRef = useRef<null | HTMLDivElement>(null)
 
-  console.log(currentFolderNode, "currfolder");
-  console.log(FileNodes, "all");
+  const handleFolderClick = () => {
+    setFolState(true); 
+    setIsDone(true)
+    setNewNode()
+
+  }
+  console.log(FileNodes,'sasa')
   return (
     <div>
-      <button onClick={() => {setFolState(true); setIsDone(true)}}>Create Folder</button>
+      <button onClick={() => console.log(treeNodeRef)}>click</button>
+      <button onClick={handleFolderClick}>Create Folder</button>
       <button onClick={() => {setFolState(false);setIsDone(true)}}>Create File</button>
       {isDone &&
        (folState !== null && (
@@ -22,9 +30,10 @@ const App = () => {
           handleIsDone={() => setIsDone(false)}
         />)
       )}
-      <div style={{ padding: "10px" }}>
-        {FileNodes.children?.map((entry) => (
-          <Entry entry={entry} depth={1} />
+      <CreateNode  isDone={isDone} folState={folState} setIsDone={setIsDone}  />
+      <div ref={treeNodeRef} style={{ padding: "10px" }}>
+        {FileNodes.children?.map((entry,idx) => (
+          <Entry entry={entry} depth={1} isDone={isDone} folState={folState} setIsDone={setIsDone} childIndex={idx} parent={FileNodes} parentExpanded={true}/>
         ))}
       </div>
     </div>
