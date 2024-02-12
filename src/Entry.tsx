5acad21d8a2bbea315f25a611840ed6e58c2a116
Void <richmondnkrumah04@gmail.com>
2024-02-12 02:45:11 +0000

    created a util helper function for newNode creation

--- src/Entry.tsx
+++ src/Entry.tsx
@@ -1,5 +1,5 @@
 // Entry.tsx
-import { useState,Dispatch,SetStateAction } from "react";
+import { useState, Dispatch, SetStateAction } from "react";
 import { useExplorerFileTree, type TFiles } from "./store";
 import File from "./File";
 import Folder from "./Folder";
@@ -8,42 +8,139 @@ import CreateNode from "./CreateNode";
 type EntryProps = {
   entry: TFiles;
   depth: number;
-  isDone: boolean,
-  setIsDone: Dispatch<SetStateAction<boolean>>
-  folState:  boolean | null,
-  childIndex: number,
-  parent: TFiles,
-  parentExpanded: boolean
+  isDone: boolean;
+  setIsDone: Dispatch<SetStateAction<boolean>>;
+  folState: boolean | null;
+  childIndex: number;
+  parent: TFiles;
+  parentExpanded: boolean;
 };
 
-const Entry = ({ entry, depth,isDone,folState,setIsDone,childIndex,parent,parentExpanded }: EntryProps) => {
+interface nodeCreation {
+  parent: TFiles;
+  currentFolderNode: string;
+  entry: TFiles;
+  parentExpanded: boolean;
+  childIndex: number;
+  newNode: boolean;
+}
+
+const Entry = ({
+  entry,
+  depth,
+  isDone,
+  folState,
+  setIsDone,
+  childIndex,
+  parent,
+  parentExpanded,
+}: EntryProps) => {
   const [isExpanded, setIsExpanded] = useState<boolean>(false);
-  const currentFolderNode = useExplorerFileTree(state => state.currentFolderNode)
-  const newNode = useExplorerFileTree(state => state.newNode)
-  console.log(entry.name,"entryy",currentFolderNode,parent.name)
-  console.log(parent.children?.length,'length',childIndex,'child')
-  console.log(parent.children?.length === childIndex +1,'childequaol')
-  console.log(newNode,'nodeQsa')
-  console.log(parent.name === currentFolderNode,'parernta')
-  console.log(parentExpanded,"expandf")
+  const currentFolderNode = useExplorerFileTree(
+    (state) => state.currentFolderNode
+  );
+  const newNode = useExplorerFileTree((state) => state.newNode);
+  console.log(
+    parent.name,
+    "parent name",
+    currentFolderNode,
+    "currentFolderNode"
+  );
+  console.log(entry.isFolder, "entry isFolder", entry.name, "entry name");
+  console.log(newNode, "newNode", parentExpanded, "parentExpanded");
+  console.log(
+    parent.children?.length,
+    "parent lenghth",
+    childIndex,
+    "childIndex"
+  );
+  console.log(parent.children?.length === childIndex + 1, "childIndex check");
+  const handleNewNode = ({
+    parent,
+    currentFolderNode,
+    entry,
+    parentExpanded,
+    childIndex,
+    newNode,
+  }: nodeCreation) => {
+    if (!parent.isFolder) return false;
+    // handle Empty Folders
+
+    // handle Folders with Children
+    if (parent.name === currentFolderNode && newNode && parentExpanded) {
+      console.log(entry.name, "this is a child of", parent.name);
+      if (parent.children?.length === childIndex + 1) return true;
+    }
+    if (currentFolderNode === entry.name && newNode && !entry.children) {
+      console.log(entry.name, "it is an empty folder");
+      return true;
+    }
+  };
   return (
     <>
-      <div style={{background: 'green', width: 'fit-content', height: 'fit-content'}}  className="flex gap-1" onClick={() => setIsExpanded((prev) => !prev)} >
-        
-        <div  
-        >
-          {entry.isFolder ? <Folder name={entry.name} /> : <File name={entry.name}/>}
-
+      <div
+        style={{
+          background: "green",
+          width: "fit-content",
+          height: "fit-content",
+        }}
+        className="flex gap-1"
+        onClick={() => setIsExpanded((prev) => !prev)}
+      >
+        <div>
+          {entry.isFolder ? (
+            <Folder name={entry.name} />
+          ) : (
+            <File name={entry.name} />
+          )}
         </div>
-        {
-          ((parent.name === currentFolderNode || entry.isFolder) && newNode && (parentExpanded ? parent.children?.length === childIndex +1 : true)) && <CreateNode isDone={isDone} folState={folState} setIsDone={setIsDone}  />
-        }
+        {/* {(parent.name === currentFolderNode ||
+          (entry.isFolder && currentFolderNode === entry.name)) &&
+          newNode &&
+          ((parent.name === currentFolderNode)
+            ? parentExpanded
+              ? parent.children?.length === childIndex + 1
+              : true
+            : true ) && (
+            <CreateNode
+              isDone={isDone}
+              folState={folState}
+              setIsDone={setIsDone}
+            />
+          )} */}
+        {handleNewNode({
+          parent,
+          currentFolderNode,
+          entry,
+          parentExpanded,
+          childIndex,
+          newNode,
+        }) && (
+          <CreateNode
+            isDone={isDone}
+            folState={folState}
+            setIsDone={setIsDone}
+          />
+        )}
+        {/* {entry.isFolder &&
+          ((entry.name === currentFolderNode || (parentExpanded && parent.children?.length === childIndex + 1)) && newNode) && (
+            <CreateNode isDone={isDone} folState={folState} setIsDone={setIsDone} />
+          )} */}
       </div>
       <div>
         {isExpanded && (
           <div style={{ paddingLeft: entry.children ? "20px" : "" }}>
-            {entry.children?.map((entryChild,idx) => (
-              <Entry entry={entryChild} depth={depth + 1} isDone={isDone} folState={folState} setIsDone={setIsDone} childIndex={idx}  parent={entry} parentExpanded={isExpanded}  />
+            {entry.children?.map((entryChild, idx) => (
+              <Entry
+                entry={entryChild}
+                depth={depth + 1}
+                isDone={isDone}
+                folState={folState}
+                setIsDone={setIsDone}
+                childIndex={idx}
+                parent={entry}
+                parentExpanded={isExpanded}
+              />
             ))}
           </div>
         )}
